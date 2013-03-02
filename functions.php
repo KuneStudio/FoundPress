0599a9220563391c8d1062f2b12e7e6c11f6b8da
Mathieu Basili - MBPRO <mathieu@kune-studio.com>
2013-03-02 12:58:31 +0100

    Added foundation support

--- functions.php
+++ functions.php
@@ -130,12 +130,24 @@ add_action( 'widgets_init', 'foundpress_widgets_init' );
 /**
  * Enqueue scripts and styles
  */
+ // Register some javascript files, because we love javascript files. Enqueue a couple as well 
+ 
+function foundpress_load_javascript_files() {
+   
+  if ( !is_admin() ) {
+  	wp_enqueue_script( 'foundation' );
+  	wp_enqueue_script( 'modernizr' );
+  	wp_enqueue_script( 'appfoundation' );
+  }
+ 
+} 
+
 function foundpress_scripts() {
 	wp_enqueue_style( 'style', get_stylesheet_uri() );
 
-	wp_enqueue_script( 'navigation', get_template_directory_uri() . '/js/navigation.js', null, '20120206', true );
+	//wp_enqueue_script( 'navigation', get_template_directory_uri() . '/js/navigation.js', null, '20120206', true );
 
-    wp_enqueue_script( 'skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array( ), '20130115', true );
+    //wp_enqueue_script( 'skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array( ), '20130115', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
